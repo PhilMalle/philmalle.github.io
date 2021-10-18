9da464c6a561ce2c191b89d71dac9af10e85d412
Ph1l M <mallephil@gmail.com>
2021-10-18 16:59:55 +0000

    Create typewriter.js

---
+++ static/js/typewriter.js
@@ -0,0 +1,74 @@
+// shamelessly lifted code http://codepen.io/hi-im-si/pen/DHoup
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+
+// for the random quote in the header
+var txtFile = new XMLHttpRequest();
+txtFile.open("GET", "/quotes.txt", true);
+txtFile.onreadystatechange = function () {
+    if (txtFile.readyState === 4) {
+        if (txtFile.status === 200) {
+            allText = txtFile.responseText;
+            lines = txtFile.responseText.split("\n");
+            randLine = lines[Math.floor((Math.random() * lines.length) + 1)];
+            document.getElementById('quote').innerHTML = randLine ||
+                "Intelligence is the ability to adapt to change."; // fallback quote
+        }
+    }
+};
+txtFile.send(null);
